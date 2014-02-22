0705ae02d1cb22169f0f746d8954cf8d2de0f645
James Jensen <james@truenorthlogic.com>
2014-02-21 22:05:26 -0700

    Initial Commit.

---
+++ CallMeMaybe/Maybe.cs
@@ -0,0 +1,9 @@
+﻿namespace CallMeMaybe
+{
+    public struct Maybe<T>
+    {
+        private T value;
+        private bool _hasValue;
+        public bool HasValue { get { return _hasValue; } }
+    }
+}
---
+++ TestMeMaybe/MaybeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CallMeMaybe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMeMaybe
+{
+    [TestClass]
+    public class MaybeTests
+    {
+        [TestMethod]
+        public void TestUninitializedValues()
+        {
+            var foo = new Foo();
+            Assert.IsNotNull(foo.Number, "Uninitialized values should not be null.");
+            Assert.IsNotNull(foo.Number, "Uninitialized values should not be null.");
+            Assert.IsFalse(foo.Number.HasValue);
+        }
+
+
+        public class Foo
+        {
+            public Maybe<int> Number { get; set; }
+            public Maybe<string> Name { get; set; }
+        }
+    }
+}
