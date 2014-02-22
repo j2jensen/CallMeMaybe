c1da5b3925c18c61ca700bc7d0b301e9fd60ac47
James Jensen <james@truenorthlogic.com>
2014-02-21 22:09:26 -0700

    Make Maybe immutable.

--- CallMeMaybe/Maybe.cs
+++ CallMeMaybe/Maybe.cs
@@ -2,8 +2,8 @@
 {
     public struct Maybe<T>
     {
-        private T value;
-        private bool _hasValue;
+        private readonly T value;
+        private readonly bool _hasValue;
         public bool HasValue { get { return _hasValue; } }
     }
 }
