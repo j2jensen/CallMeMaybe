118b5eb0882be166f406ed530907217c296967c8
James Jensen <james@truenorthlogic.com>
2014-02-22 18:09:58 -0700

    Fixed and added some linq tests

--- TestMeMaybe/MaybeLinqTests.cs
+++ TestMeMaybe/MaybeLinqTests.cs
@@ -9,16 +9,21 @@ namespace TestMeMaybe
     public class MaybeLinqTests
     {
         [Test]
-        public void TestEmptySelectLinq()
+        public void TestEmptyStringJoin()
         {
-            var q =
-                from m in Maybe.Empty<int>()
-                select m;
+            var q = Maybe.Empty<int>();
             Assert.AreEqual("", string.Join(",", q));
         }
 
         [Test]
-        public void TestSelectLinq()
+        public void TestValuedStringJoin()
+        {
+            var q = Maybe.From(1).Concat(Maybe.From(2));
+            Assert.AreEqual("1,2", string.Join(",", q));
+        }
+
+        [Test]
+        public void TestSingleLinq()
         {
             Assert.AreEqual(1, Maybe.From(1).Single());
             Assert.AreEqual("hi", Maybe.From("hi").Single());
@@ -35,5 +40,18 @@ namespace TestMeMaybe
             Assert.AreEqual(1, q.number);
             Assert.AreEqual("hi", q.name);
         }
+
+        [Test]
+        public void TestWhereLinq()
+        {
+            var q =
+                (from number in Maybe.From(1)
+                 from name in Maybe.From("hi")
+                 select new { number, name })
+                    .Single();
+            Assert.AreEqual(1, q.number);
+            Assert.AreEqual("hi", q.name);
+        }
+
     }
 }
