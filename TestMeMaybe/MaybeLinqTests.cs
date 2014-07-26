19c458d76420532b50a1ef829e765db446397012
James Jensen <jjensen@truenorthlogic.com>
2014-07-26 12:54:56 -0600

    Backed out changeset: 55746b281d00; Get rid of Concat.
    I'm having second thoughts about implementing these Concat methods at this
    point in time. There's an easy workaround, just by calling .ToList() on the
    Maybe values, and I'm thinking I'd rather focus on other priorities right now.

--- TestMeMaybe/MaybeLinqTests.cs
+++ TestMeMaybe/MaybeLinqTests.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using CallMeMaybe;
 using NUnit.Framework;
 
@@ -45,15 +44,5 @@ namespace TestMeMaybe
             Assert.AreEqual("hi", q.name);
         }
 
-        [Test]
-        public void TestConcat()
-        {
-            Assert.IsTrue(Maybe.From(1).Concat(new[] {2, 3}).SequenceEqual(new[]{1,2,3}));
-            Assert.IsTrue(Maybe.From((int?)null).Concat(new[] {2, 3}).SequenceEqual(new[]{2,3}));
-            Assert.IsTrue(Maybe.From("hi").Concat(new[] {"big", "world" })
-                .SequenceEqual(new[] { "hi", "big", "world" }));
-            Assert.IsTrue(Maybe.From((string)null).Concat(new[] { "big", "world" })
-                .SequenceEqual(new[] { "big", "world" }));
-        }
     }
 }
