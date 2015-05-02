d293e0869466e39eb9b8bf1a6a3ba33531f9eb2a
James Jensen <j2jensen+callmemaybe@gmail.com>
2015-05-02 17:29:22 -0600

    Created FirstMaybe and SingleMaybe methods.

--- CallMeMaybe/MaybeExtensions.cs
+++ CallMeMaybe/MaybeExtensions.cs
@@ -60,9 +60,232 @@ namespace CallMeMaybe
             return source.Where(m => m.HasValue).Select(m => m.Single());
         }
 
-        // TODO: MaybeFirst/MaybeLast/MaybeSingle methods
-        // TODO: MaybeSum/MaybeMax/MaybeMin methods
-        // TODO: MaybeAggregate method
+        /// <summary>
+        /// Finds the first element in the given <see cref="IQueryable{T}"/>,
+        /// and returns it as a <see cref="Maybe{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// There is no "LastMaybe()" method, because we cannot ensure the degree of performance that
+        /// calls to methods like <see cref="Queryable.First{TSource}(System.Linq.IQueryable{TSource})"/>
+        /// or <see cref="Queryable.Last{TSource}(System.Linq.IQueryable{TSource})"/> would have.
+        /// As a workaround, you may change the ordering of the <see cref="IQueryable{T}"/>, and
+        /// then call <see cref="FirstMaybe{T}(System.Linq.IQueryable{T})"/>.
+        /// </remarks>
+        /// <typeparam name="T">The type of object held by the given <see cref="IQueryable{T}"/>.</typeparam>
+        /// <param name="source">An <see cref="IQueryable{T}"/> to look for an element in.</param>
+        /// <returns>
+        /// An empty <see cref="Maybe{T}"/> if there are no elements in the given <see cref="IQueryable{T}"/>,
+        /// or if the first element is a null value.
+        /// Otherwise, returns the first element in the source.
+        /// </returns>
+        public static Maybe<T> FirstMaybe<T>(this IQueryable<T> source)
+        {
+            // When we've got an IQueryable, it may be backed by a data
+            // access layer or something, so we'll use Take() first, in hopes
+            // that it will help the underlying system to avoid loading more
+            // than one object.
+            return source.Take(1).AsEnumerable().FirstMaybe();
+        }
+
+        /// <summary>
+        /// Finds the first element in the given <see cref="IEnumerable{T}"/>,
+        /// and returns it as a <see cref="Maybe{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// There is no "LastMaybe()" method, because we cannot provide a good Queryable-based LastMaybe() method,
+        /// and we don't want people accidentally enumerating an entire <see cref="IQueryable{T}"/> because they
+        /// are accidentally calling an Enumerable-based method. 
+        /// As a workaround, you can call <code>source.Reverse().FirstMaybe()</code>, but you should be aware that
+        /// this could be expensive, depending on the nature of the underlying <see cref="IEnumerable{T}"/>
+        /// </remarks>
+        /// <typeparam name="T">The type of object held by the given <see cref="IEnumerable{T}"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to look for an element in.</param>
+        /// <returns>
+        /// An empty <see cref="Maybe{T}"/> if there are no elements in the given <see cref="IEnumerable{T}"/>,
+        /// or if the first element is a null value.
+        /// Otherwise, returns the first element in the source.
+        /// </returns>
+        public static Maybe<T> FirstMaybe<T>(this IEnumerable<T> source)
+        {
+            var enumerator = source.GetEnumerator();
+            return enumerator.MoveNext()
+                ? new Maybe<T>(enumerator.Current)
+                : new Maybe<T>(); 
+        }
+
+
+        /// <summary>
+        /// Finds the first element in the given <see cref="IQueryable{T}"/>,
+        /// and returns it as a <see cref="Maybe{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// There is no "LastMaybe()" method, because we cannot ensure the degree of performance that
+        /// calls to methods like <see cref="Queryable.First{TSource}(System.Linq.IQueryable{TSource})"/>
+        /// or <see cref="Queryable.Last{TSource}(System.Linq.IQueryable{TSource})"/> would have.
+        /// As a workaround, you may change the ordering of the <see cref="IQueryable{T}"/>, and
+        /// then call <see cref="FirstMaybe{T}(System.Linq.IQueryable{T})"/>.
+        /// </remarks>
+        /// <typeparam name="T">The type of object held by the given <see cref="IQueryable{T}"/>.</typeparam>
+        /// <param name="source">An <see cref="IQueryable{T}"/> to look for an element in.</param>
+        /// <returns>
+        /// An empty <see cref="Maybe{T}"/> if there are no elements in the given <see cref="IQueryable{T}"/>,
+        /// or if the first element is a null value.
+        /// Otherwise, returns the first element in the source.
+        /// </returns>
+        public static Maybe<T> FirstMaybe<T>(this IQueryable<T?> source)
+            where T : struct 
+        {
+            // When we've got an IQueryable, it may be backed by a data
+            // access layer or something, so we'll use Take() first, in hopes
+            // that it will help the underlying system to avoid loading more
+            // than one object.
+            return source.Take(1).AsEnumerable().FirstMaybe();
+        }
+
+        /// <summary>
+        /// Finds the first element in the given <see cref="IEnumerable{T}"/>,
+        /// and returns it as a <see cref="Maybe{T}"/>.
+        /// </summary>
+        /// <remarks>
+        /// There is no "LastMaybe()" method, because we cannot provide a good Queryable-based LastMaybe() method,
+        /// and we don't want people accidentally enumerating an entire <see cref="IQueryable{T}"/> because they
+        /// are accidentally calling an Enumerable-based method. 
+        /// As a workaround, you can call <code>source.Reverse().FirstMaybe()</code>, but you should be aware that
+        /// this could be expensive, depending on the nature of the underlying <see cref="IEnumerable{T}"/>
+        /// </remarks>
+        /// <typeparam name="T">The type of object held by the given <see cref="IEnumerable{T}"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to look for an element in.</param>
+        /// <returns>
+        /// An empty <see cref="Maybe{T}"/> if there are no elements in the given <see cref="IEnumerable{T}"/>,
+        /// or if the first element is a null value.
+        /// Otherwise, returns the first element in the source.
+        /// </returns>
+        public static Maybe<T> FirstMaybe<T>(this IEnumerable<T?> source)
+            where T : struct
+        {
+            var enumerator = source.GetEnumerator();
+            return enumerator.MoveNext()
+                ? enumerator.Current.Maybe()
+                : new Maybe<T>();
+        }
+
+        // Note: I've made a conscious decision not to implement a LastMaybe 
+        // method. It would open the door for someone to accidentally invoke
+        // it on an IQueryable, and there's no reliable way to ensure that
+        // the IQueryable only requests the last item from its data store.
+        // If the user has an in-memory collection, they should be able
+        // to .Reverse().FirstMaybe() it. If not, they're probably doing
+        // an .OrderBy() to make .First() meaningful--they'll just have to
+        // .OrderByDescending().FirstMaybe() instead.
+
+        /// <summary>
+        /// Finds the only element in this sequence, and returns it as a <see cref="Maybe{T}"/>.
+        /// If there is more than one element in the sequence, an <see cref="InvalidOperationException"/>
+        /// will be thrown.
+        /// </summary>
+        /// <typeparam name="T">The type of object held by the given <see cref="IEnumerable{T}"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to look for an element in.</param>
+        /// <returns>
+        /// An empty <see cref="Maybe{T}"/> if there are no elements in the given <see cref="IQueryable{T}"/>,
+        /// or if the first element is a null value.
+        /// Otherwise, returns the only element in the source.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <paramref name="source"/> contains more than one element.
+        /// </exception>
+        public static Maybe<T> SingleMaybe<T>(this IQueryable<T> source)
+        {
+            // When we've got an IQueryable, it may be backed by a data
+            // access layer or something, so we'll use Take() first, in hopes
+            // that it will help the underlying system to avoid loading more
+            // objects than necessary.
+            return source.Take(2).AsEnumerable().SingleMaybe();
+        }
+
+        /// <summary>
+        /// Attempts to find the only element in the given sequence.
+        /// </summary>
+        /// <typeparam name="T">The type of object held by the given <see cref="IEnumerable{T}"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to look for an element in.</param>
+        /// <returns>
+        /// An empty <see cref="Maybe{T}"/> if there are no elements in the given <see cref="IEnumerable{T}"/>,
+        /// or if the first element is a null value.
+        /// Otherwise, returns the only element in the source.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <paramref name="source"/> contains more than one element.
+        /// </exception>
+        public static Maybe<T> SingleMaybe<T>(this IEnumerable<T> source)
+        {
+            var enumerator = source.GetEnumerator();
+            if (!enumerator.MoveNext())
+            {
+                return new Maybe<T>();
+            }
+            T value = enumerator.Current;
+            if (enumerator.MoveNext())
+            {
+                throw new InvalidOperationException("Sequence contains more than one element");
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Finds the only element in this sequence, and returns it as a <see cref="Maybe{T}"/>.
+        /// If there is more than one element in the sequence, an <see cref="InvalidOperationException"/>
+        /// will be thrown.
+        /// </summary>
+        /// <typeparam name="T">The type of object held by the given <see cref="IEnumerable{T}"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to look for an element in.</param>
+        /// <returns>
+        /// An empty <see cref="Maybe{T}"/> if there are no elements in the given <see cref="IQueryable{T}"/>,
+        /// or if the first element is a null value.
+        /// Otherwise, returns the only element in the source.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <paramref name="source"/> contains more than one element.
+        /// </exception>
+        public static Maybe<T> SingleMaybe<T>(this IQueryable<T?> source)
+            where T : struct 
+        {
+            // When we've got an IQueryable, it may be backed by a data
+            // access layer or something, so we'll use Take() first, in hopes
+            // that it will help the underlying system to avoid loading more
+            // objects than necessary.
+            return source.Take(2).AsEnumerable().SingleMaybe();
+        }
+
+        /// <summary>
+        /// Attempts to find the only element in the given sequence.
+        /// </summary>
+        /// <typeparam name="T">The type of object held by the given <see cref="IEnumerable{T}"/>.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to look for an element in.</param>
+        /// <returns>
+        /// An empty <see cref="Maybe{T}"/> if there are no elements in the given <see cref="IEnumerable{T}"/>,
+        /// or if the first element is a null value.
+        /// Otherwise, returns the only element in the source.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the <paramref name="source"/> contains more than one element.
+        /// </exception>
+        public static Maybe<T> SingleMaybe<T>(this IEnumerable<T?> source)
+            where T : struct
+        {
+            var enumerator = source.GetEnumerator();
+            if (!enumerator.MoveNext())
+            {
+                return new Maybe<T>();
+            }
+            T? value = enumerator.Current;
+            if (enumerator.MoveNext())
+            {
+                throw new InvalidOperationException("Sequence contains more than one element");
+            }
+            return value.Maybe();
+        }
+
+        // TODO: SumMaybe/MaxMaybe/MinMaybe methods
+        // TODO: AggregateMaybe method
 
         /// <summary>
         /// Converts the given nullable value into a <see cref="Maybe{T}"/>
--- TestMeMaybe/MaybeExtensionsTests.cs
+++ TestMeMaybe/MaybeExtensionsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 using CallMeMaybe;
 using NUnit.Framework;
@@ -13,11 +14,11 @@ namespace TestMeMaybe
         {
             var values = new[] {1, 2, 4};
             var dict = values.ToDictionary(v => v.ToString(CultureInfo.InvariantCulture));
-            var values2 = 
+            var values2 =
                 (from k in new[] {"1", "2", "3"}
-                from v in dict.GetMaybe(k)
-                select new {k, v})
-                .ToList();
+                    from v in dict.GetMaybe(k)
+                    select new {k, v})
+                    .ToList();
             Assert.AreEqual(values2.Count, 2);
             Assert.AreEqual(1, values2[0].v);
             Assert.AreEqual("1", values2[0].k);
@@ -30,8 +31,68 @@ namespace TestMeMaybe
         {
             Assert.AreEqual("1", UseNullable(Maybe.From(1).Nullable()));
             Assert.AreEqual("", UseNullable(Maybe<int>.Not.Nullable()));
-            Assert.AreEqual("1", UseMaybe(((int?)1).Maybe()));
-            Assert.AreEqual("", UseMaybe(((int?)null).Maybe()));
+            Assert.AreEqual("1", UseMaybe(((int?) 1).Maybe()));
+            Assert.AreEqual("", UseMaybe(((int?) null).Maybe()));
+        }
+
+        [Test]
+        public void TestFirstMaybe()
+        {
+            Assert.AreEqual(Maybe.From(1), new[] {1}.FirstMaybe());
+            Assert.AreEqual(Maybe.From(1), new[] {1}.AsQueryable().FirstMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int?[] {null}.FirstMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int?[] {null}.AsQueryable().FirstMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int?[] {null, 2, 3}.FirstMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int?[] {null, 2, 3}.AsQueryable().FirstMaybe());
+            Assert.AreEqual(Maybe.From(1), new[] {1, 2, 3}.FirstMaybe());
+            Assert.AreEqual(Maybe.From(1), new[] {1, 2, 3}.AsQueryable().FirstMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int[0].FirstMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int[0].AsQueryable().FirstMaybe());
+
+            Assert.AreEqual(Maybe.From("1"), new[] {"1"}.FirstMaybe());
+            Assert.AreEqual(Maybe.From("1"), new[] {"1"}.AsQueryable().FirstMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new string[] {null}.FirstMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new string[] {null}.AsQueryable().FirstMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new[] {null, "2", "3"}.FirstMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new[] {null, "2", "3"}.AsQueryable().FirstMaybe());
+            Assert.AreEqual(Maybe.From("1"), new[] {"1", "2", "3"}.FirstMaybe());
+            Assert.AreEqual(Maybe.From("1"), new[] {"1", "2", "3"}.AsQueryable().FirstMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new string[0].FirstMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new string[0].AsQueryable().FirstMaybe());
+        }
+
+        [Test]
+        public void TestSingleMaybe()
+        {
+            Assert.AreEqual(Maybe.From(1), new[] {1}.SingleMaybe());
+            Assert.AreEqual(Maybe.From(1), new[] {1}.AsQueryable().SingleMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int?[] { null }.SingleMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int?[] { null }.AsQueryable().SingleMaybe());
+            Assert.Throws<InvalidOperationException>(() => new int?[] { null, 2, 3 }.SingleMaybe(),
+                "Sequence contains more than one element");
+            Assert.Throws<InvalidOperationException>(() => new int?[] { null, 2, 3 }.AsQueryable().SingleMaybe(),
+                "Sequence contains more than one element");
+            Assert.Throws<InvalidOperationException>(() => new[] { 1, 2, 3 }.SingleMaybe(),
+                "Sequence contains more than one element");
+            Assert.Throws<InvalidOperationException>(() => new[] {1, 2, 3}.AsQueryable().SingleMaybe(),
+                "Sequence contains more than one element");
+            Assert.AreEqual(Maybe<int>.Not, new int[0].SingleMaybe());
+            Assert.AreEqual(Maybe<int>.Not, new int[0].AsQueryable().SingleMaybe());
+
+            Assert.AreEqual(Maybe.From("1"), new[] {"1"}.SingleMaybe());
+            Assert.AreEqual(Maybe.From("1"), new[] {"1"}.AsQueryable().SingleMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new string[] {null}.SingleMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new string[] {null}.AsQueryable().SingleMaybe());
+            Assert.Throws<InvalidOperationException>(() => new [] { null, "2", "3" }.SingleMaybe(),
+                "Sequence contains more than one element");
+            Assert.Throws<InvalidOperationException>(() => new [] { null, "2", "3" }.AsQueryable().SingleMaybe(),
+                "Sequence contains more than one element");
+            Assert.Throws<InvalidOperationException>(() => new[] { "1", "2", "3" }.SingleMaybe(),
+                "Sequence contains more than one element");
+            Assert.Throws<InvalidOperationException>(() => new[] {"1", "2", "3"}.AsQueryable().SingleMaybe(),
+                "Sequence contains more than one element");
+            Assert.AreEqual(Maybe<string>.Not, new string[0].SingleMaybe());
+            Assert.AreEqual(Maybe<string>.Not, new string[0].AsQueryable().SingleMaybe());
         }
 
         private string UseNullable(int? x)
