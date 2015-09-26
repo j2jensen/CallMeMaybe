3634570b61fa295ae857e4f4dea2b5b7d06f028f
James Jensen <j2jensen+callmemaybe@gmail.com>
2015-09-25 21:29:58 -0600

    Finished documentation for MaybeNot.

--- CallMeMaybe/MaybeNot.cs
+++ CallMeMaybe/MaybeNot.cs
@@ -14,17 +14,28 @@ namespace CallMeMaybe
             return "";
         }
 
+        /// <summary>
+        /// An <see cref="Equals"/> override, which will only return <value>true</value> if
+        /// the object is another <see cref="MaybeNot"/>.
+        /// </summary>
+        /// <remarks>
+        /// Each separate type of Maybe counts as its own value.
+        /// All <see cref="MaybeNot"/>s are the same, but different <see cref="Maybe{T}.Not"/>s
+        /// are their own values.
+        /// This is just to maintain consistent behavior--users are not expected to put different types of Maybe objects
+        /// in hashsets and such.
+        /// </remarks>
+        /// <param name="obj">The object to compare this one with.</param>
+        /// <returns><value>true</value> if the given value is a <see cref="MaybeNot"/>, <value>false</value> otherwise.</returns>
         public override bool Equals(object obj)
         {
-            // Each separate type of Maybe counts as its own value.
-            // All MaybeNots are the same, but different Maybe<>.Nots
-            // are their own values.
-            // This is just to maintain consistent behavior--users are
-            // not expected to put different types of Maybe objects
-            // in hashsets and such.
             return obj is MaybeNot;
         }
 
+        /// <summary>
+        /// A <see cref="GetHashCode"/> override.
+        /// </summary>
+        /// <returns>A hashcode which is guaranteed to be the same for all <see cref="MaybeNot"/>s.</returns>
         public override int GetHashCode()
         {
             unchecked
@@ -41,11 +52,11 @@ namespace CallMeMaybe
 
         /// <summary>
         /// Attempts to get the value.
+        /// </summary>
         /// <remarks>
         /// For <see cref="MaybeNot"/>, this will always return false, 
         /// and produce a null output value.
         /// </remarks>
-        /// </summary>
         /// <param name="value">
         /// In the case of <see cref="MaybeNot"/>, this will always yield null.
         /// </param>
