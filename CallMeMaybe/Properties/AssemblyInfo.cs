44f6a798bde8e3dd208da715000d141c4485f8c9
James Jensen <j2jensen@gmail.com>
2016-05-11 16:06:35 -0600

    Marked CallMeMaybe CLSCompliant

--- CallMeMaybe/Properties/AssemblyInfo.cs
+++ CallMeMaybe/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
-﻿using System.Resources;
+﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -17,14 +16,15 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
-//
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
-//
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.5.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Avoid CA1014(https://msdn.microsoft.com/library/ms182156.aspx)
+[assembly: CLSCompliant(true)]
