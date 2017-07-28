6c596b91eef08a4f2818b84306155ec69752e867
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-07-28 10:59:37 -0700

    Set default version to .yml file

--- build.cmd
+++ build.cmd
@@ -11,8 +11,8 @@ IF NOT EXIST Version.txt (
 )
 
 :: get the version and comment from Version.txt lines 2 and 3
-SET RELEASE=
-SET COMMENT=
+SET RELEASE=%APPVEYOR_BUILD_VERSION%
+SET COMMENT=%MAYBE_PRERELEASE_SUFFIX%
 FOR /F "skip=1 delims=" %%i IN (Version.txt) DO IF NOT DEFINED RELEASE SET RELEASE=%%i
 FOR /F "skip=2 delims=" %%i IN (Version.txt) DO IF NOT DEFINED COMMENT SET COMMENT=%%i
 
